1737d2093b69ee6cb92a9f4b0af907163a3fa899
Matthew M Tarfa <tarfamatthew@gmail.com>
2022-11-24 22:07:42 +0000

    added a statement in the final print.

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ city_name=input("what is the name of the city you grew up\n")
 #3. Ask the user for the name of a pet.
 pet_name=input("what is your pet name or favorite kind of pet?\n")
 #4. Combine the name of their city and pet and show them their business name.
-print(city_name + pet_name)
+print("Your business name is "+ city_name + pet_name)
