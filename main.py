c105e5b4eba91c1e993d0cc5449d9c374a573651
Matthew M Tarfa <tarfamatthew@gmail.com>
2022-11-24 22:00:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+
+print("welcome to TM's world buniness name builder")
+
+#2. Ask the user for the city that they grew up in.
+city_name=input("what is the name of the city you grew up\n")
+
+#3. Ask the user for the name of a pet.
+pet_name=input("what is your pet name or favorite kind of pet?\n")
+#4. Combine the name of their city and pet and show them their business name.
+print(city_name + pet_name)
